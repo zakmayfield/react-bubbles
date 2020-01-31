4676a20ac8c40bbf146224a9179765b0e300c22a
Zachary Mayfield <zakmayfield@gmail.com>
2020-01-31 11:11:27 -0500

    created a log in form that does NOT make an POST request yet -- controlled inputs and such

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,55 @@
 import React from "react";
+import axios from 'axios';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    })
+    console.log(e.target.name, '=', e.target.value)
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('log in button clicked!')
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Log in Here</h2>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+
+          <input
+            type="text"
+            name="password"
+            placeholder="Password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log In</button>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default Login;
